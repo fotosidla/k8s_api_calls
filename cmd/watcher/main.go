8717d6b2a70000641883360af0694a9ebe23903a
Pavel Šidla <fotosidla@gmail.com>
2021-11-27 10:30:00 +0100

    main reworked

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/go-redis/redis"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
@@ -17,13 +14,115 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-type Output struct {
+type app struct {
+	client *kubernetes.Clientset
+	redis  *kubernetes.Clientset
+	demo   demo
+}
+
+type demo struct {
+	client *kubernetes.Clientset
+}
+
+func main() {
+	ctx := context.Background()
+	events, err := connK8s().CoreV1().Events(v1.NamespaceAll).Watch(ctx, metav1.ListOptions{})
+	app := &app{
+		client: connK8s(),
+	}
+	if err != nil {
+		panic(err)
+	}
+	resultChan := events.ResultChan()
+	app.processEvents(resultChan)
+	pods, err := connK8s().CoreV1().Pods("").List(ctx, metav1.ListOptions{})
+	app.getOwner(pods)
+	println(pods.Items)
+
+}
+
+func (c *app) processEvents(events <-chan watch.Event) {
+	for event := range events {
+		mallEvent := c.mapEvent(event.Object.(*v1.Event))
+
+		if mallEvent == nil {
+			fmt.Println("Skipped processing of event.")
+			continue
+		}
+
+		c.storeEvent(mallEvent)
+
+	}
+}
+
+func (asdf *app) storeEvent(event *MallEvent) {
+	fmt.Println("Stored mall event.")
+
+	// TODO store logic
+}
+
+type MallEvent struct {
+	owner   string `json:"owner"`
+	kind    string `json:"kind"`
 	Name    string `json:"name"`
 	Time    string `json:"time"`
 	Reason  string `json:"reason"`
 	Message string `json:"message"`
+	// TODO some other fields
 }
 
+func (c *app) getOwner(pods *v1.PodList) *MallEvent {
+
+	cAPP := c.client.AppsV1()
+	for _, pod := range pods.Items {
+		if len(pod.OwnerReferences) == 0 {
+			return nil
+
+		}
+
+		switch pod.OwnerReferences[0].Kind {
+		case "ReplicaSet":
+			replica, replicaERR := cAPP.ReplicaSets(pod.Namespace).Get(context.TODO(), pod.OwnerReferences[0].Name, metav1.GetOptions{})
+			if replicaERR != nil {
+				panic(replicaERR.Error())
+			}
+			return &MallEvent{
+				owner: replica.OwnerReferences[0].Name,
+				kind:  "Deployment",
+			}
+		case "DaemonSet", "StatefulSet":
+			return &MallEvent{
+				owner: pod.OwnerReferences[0].Name,
+				kind:  pod.OwnerReferences[0].Kind,
+			}
+		default:
+			continue
+		}
+	}
+	return nil
+	}
+
+
+func (c *app) mapEvent(event *v1.Event) *MallEvent {
+
+	fmt.Println("Mapping mall event.")
+
+	// TODO example just for illustration of limited mapping support
+	if event.Reason != "Scheduled" {
+		return nil
+	}
+
+	return &MallEvent{
+		Name:    event.Name,
+		Time:    event.EventTime.Format("2 Jan 2006 15:04:05"),
+		Reason:  event.Reason,
+		Message: event.Message,
+	}
+
+	// TODO some mapping logic
+}
+
+// TODO extract into shared unit FIXME copypaste
 func connK8s() *kubernetes.Clientset {
 	kubeconfig := filepath.Join(
 		os.Getenv("HOME"), ".kube", "config",
@@ -37,99 +136,4 @@ func connK8s() *kubernetes.Clientset {
 		log.Fatal(err)
 	}
 	return clientset
-}
-
-func GetEventsNSA(clientset *kubernetes.Clientset, client *redis.Client, ctx context.Context) {
-
-	api := clientset.CoreV1()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	watcher, err := api.Events(v1.NamespaceAll).Watch(ctx, metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-	for event := range watcher.ResultChan() {
-		svc := event.Object.(*v1.Event)
-		switch event.Type {
-		case watch.Added:
-			if svc.Reason == "Scheduled" {
-				bytes, err := json.Marshal(Output{
-					Name:    svc.Name,
-					Time:    svc.EventTime.Format("2 Jan 2006 15:04:05"),
-					Reason:  svc.Reason,
-					Message: svc.Message,
-				})
-				if err != nil {
-					panic(err)
-				}
-				err = client.Set(string(svc.UID), bytes, 0).Err()
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(svc.Name, svc.Reason, svc.InvolvedObject.ResourceVersion)
-			}
-		}
-	}
-
-}
-
-func PodEvents(clientset *kubernetes.Clientset) {
-	api := clientset.CoreV1()
-	papi := clientset.AppsV1()
-	pods, err := api.Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	for _, pod := range pods.Items {
-		if len(pod.OwnerReferences) == 0 {
-			fmt.Printf("NO OWNER", pod.Name)
-			continue
-		}
-		var ownerName, ownerKind string
-		switch pod.OwnerReferences[0].Kind {
-		case "ReplicaSet":
-			replica, repErr := papi.ReplicaSets(pod.Namespace).Get(context.TODO(), pod.OwnerReferences[0].Name, metav1.GetOptions{})
-			if repErr != nil {
-				panic(repErr.Error())
-			}
-			ownerName = replica.OwnerReferences[0].Name
-			ownerKind = "Deployment"
-		case "DaemonSet", "StatefulSet":
-			ownerName = pod.OwnerReferences[0].Name
-			ownerKind = pod.OwnerReferences[0].Kind
-		default:
-			continue
-		}
-		fmt.Printf("POD %s is managed by %s %s\n", pod.Name, ownerName, ownerKind)
-	}
-
-}
-
-func main() {
-	// uncomment to use alternative implementation
-	// watcher.Main()
-	//TODO: Jak na flagy? -> nefungují ani po kompilaci ani při go run main.go
-
-	//REDIS CONNECTION
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
-
-	//Redis test connection
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-
-	var ns string
-	flag.StringVar(&ns, "namespace", "", "namespace")
-	flag.Parse()
-
-	// Popis cesty k kube configu - HOME/.kube/config
-	// Filepath join -> vytvoří cestu ze zadaných stringů
-	ctx := context.Background()
-	clientset := connK8s()
-	PodEvents(clientset)
-	GetEventsNSA(clientset, client, ctx)
-
 }
--- pkg/watcher/alternative.go
+++ pkg/watcher/alternative.go
@@ -3,59 +3,83 @@ package watcher
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"log"
-	"os"
-	"path/filepath"
 )
+
+type app struct {
+	client *kubernetes.Clientset
+}
+
+type demo struct {
+	client *kubernetes.Clientset
+}
 
 func Main() {
 	ctx := context.Background()
 	events, err := connK8s().CoreV1().Events(v1.NamespaceAll).Watch(ctx, metav1.ListOptions{})
+	app := &app{
+		client: connK8s(),
+	}
 	if err != nil {
 		panic(err)
 	}
 	resultChan := events.ResultChan()
-	processEvents(resultChan)
+	app.processEvents(resultChan)
 }
 
-func processEvents(events <-chan watch.Event) {
+func (c *app) processEvents(events <-chan watch.Event) {
 	for event := range events {
-		mallEvent := mapEvent(event.Object.(*v1.Event))
+		mallEvent := c.mapEvent(event.Object.(*v1.Event))
 		if mallEvent == nil {
 			fmt.Println("Skipped processing of event.")
 			continue
 		}
-		storeEvent(mallEvent)
+		//storeEvent(mallEvent)
 	}
 }
 
-func storeEvent(event *MallEvent) {
+func (asdf *app) storeEvent(event *MallEvent) {
 	fmt.Println("Stored mall event.")
+
 	// TODO store logic
 }
 
 type MallEvent struct {
-	deployment  string
-	otherFields string
+	deployment  string `json:"deployment"`
+	otherFields string `json:"otherFields"`
+	Name        string `json:"name"`
+	Time        string `json:"time"`
+	Reason      string `json:"reason"`
+	Message     string `json:"message"`
 	// TODO some other fields
 }
 
-func mapEvent(event *v1.Event) *MallEvent {
+func (c *app) mapEvent(event *v1.Event) *MallEvent {
+	// c.client.AppsV1().ReplicaSets().Apply()
+
 	fmt.Println("Mapping mall event.")
+
 	// TODO example just for illustration of limited mapping support
 	if event.Reason != "Scheduled" {
 		return nil
+	} else {
+		println("name", event.Name, "message", event.Message)
+		return &MallEvent{
+			Name:    event.Name,
+			Time:    event.EventTime.Format("2 Jan 2006 15:04:05"),
+			Reason:  event.Reason,
+			Message: event.Message,
+		}
 	}
 	// TODO some mapping logic
-	return &MallEvent{
-		deployment:  "TODO somehow",
-		otherFields: "TODO something else",
-	}
 }
 
 // TODO extract into shared unit FIXME copypaste
